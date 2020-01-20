6a2c777b73de169d79c63357d0ee08c006dfa070
justine66 <47179968+justine66@users.noreply.github.com>
2020-01-20 17:07:34 +0100

    V2.2

--- p1.ml
+++ p1.ml
@@ -14,7 +14,9 @@ let start nom_fichier nom_soluce =
 	  solution.(i/9).(i mod 9) <- b.[i];    
                          
     done
-  in split()
+  in split();
+  close_in files;
+  close_in soluce;;
     
 
 let affichage g =
@@ -41,6 +43,31 @@ let check () =
 
 let check_case i j = if grille.(i).(j) == solution.(i).(j) then true else false;;
 		
+let save  = 
+	 Printf.printf "donnez un nom a votre fichier";
+	let a = Scanf.scanf " %s" (fun x -> x); 
+	let save_file = open_out a^".txt" in
+	for i = 0 to (Array.length g)-1  do
+		for j = 0 to (Array.length g.(0))-1  do
+			output save_file  g.(i).(j) 0 1;
+		done
+	done;
+	(*close_out save_file;*)
+	let b = [];
+	let file = open_in "sauvegarde.txt" in 
+	for i=0 to (in_channel_length file) do 
+		b::input_line file
+	done;
+	(*close_in "sauvegarde.txt";*)
+	name_file::b;
+	save_name b
+	
+let rec save_name b =
+	let file = open_out "sauvegarde.txt" in 
+		match b with
+		|hd::tl -> output file hd 0 (String.length hd); save_name tl
+		|[] -> close_out file
+	
 		
 
 let () =
@@ -48,6 +75,7 @@ let () =
 	affichage grille;
 	Printf.printf "\n";
 	Printf.printf "%b\n" (check ());
+	save
 
 
 	
