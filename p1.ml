f424d24a7f45064886eb91125ec93ba8bb293ac3
justine66 <47179968+justine66@users.noreply.github.com>
2020-02-10 13:36:12 +0100

    V3.1

--- p1.ml
+++ p1.ml
@@ -1,24 +1,32 @@
+open Str
+
+
 type case = { mutable modifiable : bool;
 			  mutable valeur     : char}
 			  
 (*let grille = Array.make_matrix 9 9 (ref{modifiable = true ; valeur = '0'})*)
 let grille = Array.make_matrix 9 9 {modifiable = true ; valeur = '0'}
 let solution = Array.init 9 (fun i -> Array.make 9 '0')
+let fichier_solution = ref "c"
 
 let start nom_fichier nom_soluce = 
   
-  let files = open_in ("grids/"^nom_fichier^".txt") in 
-  let soluce = open_in ("solutions/"^nom_soluce^".txt") in 
+  fichier_solution := nom_soluce ;
+  (*Printf.printf "%s" !fichier_solution;*)
+  let files = open_in ("grids/"^nom_fichier) in 
+  let soluce = open_in ("solutions/"^nom_soluce) in 
   let split () =
     let a = input_line files in
     let b = input_line soluce in
+    
     for i = 0 to (String.length a)-1  do
-      (*grille.(i/9).(i mod 9) := { modifiable = true; valeur = a.[i]};*)
-      grille.(i/9).(i mod 9) <- { modifiable = true; valeur = a.[i]};
+      grille.(i/9).(i mod 9) <- a.[i];        
 	  solution.(i/9).(i mod 9) <- b.[i];    
                          
     done
-  in split()
+  in split();
+  close_in files;
+  close_in soluce;;
     
 
 let affichage g =
@@ -26,13 +34,13 @@ let affichage g =
   for i = 0 to (Array.length g)-1  do
      for j = 0 to (Array.length g.(0))-1  do
     
-      Printf.printf "%c" g.(i).(j).valeur
+      Printf.printf "%c" g.(i).(j)
                     done
    
-  done
+  done;;
  
         
-(*let check () = 
+let check () = 
 	let rec check_rec  i j = 
 		match i with
 		|8 -> (match j with
@@ -43,13 +51,74 @@ let affichage g =
 			|_ -> if grille.(i).(j) == solution.(i).(j) then check_rec i (j+1) else false)
 	in check_rec 0 0;;
 
-let check_case i j = if grille.(i).(j) == solution.(i).(j) then true else false;;*)
+let check_case i j = if grille.(i).(j) == solution.(i).(j) then true else false;;
+	
+let rec save_name bd =
+		(*Printf.printf "Test\n";*)
+		let file = open_out "sauvegarde.txt" in 
+		let rec save_b bc = 
+			match bc with
+			|hd::tl -> (*Printf.fprintf file "%s\n" hd;*) save_b tl
+			|[] -> ()
+		in save_b bd;
+		close_out file;;
 		
-let grillePourAffichage ()=
-	start "gridTest" "solution0" ;
+let save_n a b =
+	let file = open_in "sauvegarde.txt" in 
+	let rec save_n_aux bb =
 
-(*let () =
-	start "grid0" "solution0" ;
-	affichage grille;
+		try
+			let c = input_line file in 
+			save_n_aux (c::bb); (*Printf.printf "test\n" ; if (i == (in_channel_length file)) then a::b else b*)
+			
+		with
+			|End_of_file -> save_name ((a^"/"^(!fichier_solution))::List.rev(bb));
+	in save_n_aux b;
+	close_in file;;
+			
+let save () = 
+		Printf.printf "donnez un nom a votre fichier\n%!";
+	let a = Scanf.scanf "%s"(fun x -> x^".txt"); in
+	let save_file = open_out ("grids/"^a) in
+	for i = 0 to (Array.length grille)-1  do
+		for j = 0 to (Array.length grille.(0))-1  do
+			Printf.fprintf save_file "%c" grille.(i).(j); 
+		done
+	done;
+	close_out save_file;
+	save_n a [];;
+
+let charge () = 
+		let charge_file = open_in ("sauvegarde.txt") in
+		let b= ref [] in
+		let rec charge_aux b cf =
+			try
+				let sp = Str.split (Str.regexp "/") (input_line cf) in 
+				b := (List.hd sp)::!b;
+				charge_aux b cf;
+				(*Printf.printf "%s\n" (List.hd sp);*)
+				
+			with
+				|End_of_file -> !b; close_in charge_file;
+		in charge_aux b charge_file
+		 ;;
+		 
+let regles() =
+	let r = "Les Règles" in
+	let rg = "Remplir les cases vides avec les chiffres de 1 à 9, de telle sorte qu'ils n'apparaissent qu'une fois par ligne, par colonne et par carré de 3x3 cases." in 
+	let c = "Les Commandes" in 
+	let c1 = "q: exit" in 
+			Printf.printf "%s\n %s\n %s\n %s\n " r rg c c1
+		
+	
+let grillePourAffichage ()=
+	start "grid2.txt" "solution0.txt";;
+	
+let () =
+	start "grid0.txt" "solution0.txt" ;
+	(*affichage grille;*)
 	Printf.printf "\n";
-	Printf.printf "%b\n" (check ())*)
+	(*save();*)
+	(*Printf.printf "SOLUTION : %s\n" !fichier_solution;*)
+	(*charge()*)
+
